0034f2d11cc315f17c2c9e32fc09f5849a290052
Elijah Yap <yap@usc.edu>
2021-05-01 01:23:46 -0700

    remove unused parts of template

--- main.ts
+++ main.ts
@@ -2,28 +2,9 @@ import { App, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } f
 import CodeMirror from 'codemirror';
 import replaceText from 'replaceText';
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
 export default class Chisel extends Plugin {
-	settings: MyPluginSettings;
-    cm: CodeMirror.Editor;
-
 	async onload() {
 		console.log('loading plugin');
-
-		await this.loadSettings();
-
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
-
-		this.addStatusBarItem().setText('Status Bar Text');
 
 		this.addCommand({
 			id: 'insert-pagebreak',
@@ -40,10 +21,7 @@ export default class Chisel extends Plugin {
 			}
 		});
 
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
 		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-            this.cm = cm;
 			console.log('codemirror', cm);
 		});
 
@@ -56,58 +34,5 @@ export default class Chisel extends Plugin {
 
 	onunload() {
 		console.log('unloading plugin');
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: Chisel;
-
-	constructor(app: App, plugin: Chisel) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		let {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
